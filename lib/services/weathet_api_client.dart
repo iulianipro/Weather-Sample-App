595a71c055a4267a4d7cf4e365b8959bbd431b51
Julian <59797376+iulianipro@users.noreply.github.com>
2022-11-14 23:01:19 +0100

    Update weathet_api_client.dart

--- lib/services/weathet_api_client.dart
+++ lib/services/weathet_api_client.dart
@@ -6,7 +6,7 @@ import 'package:sampleprojekt/model/weather_model.dart';
 class WeatherApiClinet {
   Future<Weather>? getCurrentWeather(String? location) async {
     var endpoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=$location&units=metric&appid=a4968bbea35fe64b1d8fe5440309573e");
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&units=metric&appid={API Key}");
 
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
